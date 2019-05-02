850adb3b1e629af00a18a745b9577a19063671c5
Vegard <vgsjaavik@gmail.com>
2019-05-02 16:43:31 +0000

    .

--- Rpi_music.py
+++ Rpi_music.py
@@ -4,14 +4,16 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(10, GPIO.IN)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def checkInput():
-    if(GPIO.input(10)):
+    print(GPIO.input(4))	
+    if(GPIO.input(4)):
         print("Ready to ROLL!")
+	playSong()
     else:
-        print("Waiting for buttonpress")
-        os.system("clear")
+       print("Waiting for buttonpress")
+       os.system("clear")
 
 def playSong():
     os.system("play Sang.wav")  
