2defc59dd4be9992cc7921cbd86b03ecd7ce4e7e
Vegard <vgsjaavik@gmail.com>
2019-05-24 23:31:12 +0200

    Added several songs

--- Rpi_music.py
+++ Rpi_music.py
@@ -1,10 +1,13 @@
 import os
 import RPi.GPIO as GPIO
 import time
+import random
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+songs = ["/etc/Musicplayer/Sang.wav", "/etc/Musicplayer/TNT.wav", "/etc/Musicplayer/Minecraft_Style.wav"]
 
 def checkInput():
     print(GPIO.input(4))	
@@ -15,7 +18,8 @@ def checkInput():
        os.system("clear")
     time.sleep(1)
 def playSong():
-    os.system("play /etc/Musicplayer/Sang.wav")  
+    songNumber = random.randint(0,2)
+    os.system("play" + songs[songNumber])  
     os.system("echo 'finish'")
 
 while True:
