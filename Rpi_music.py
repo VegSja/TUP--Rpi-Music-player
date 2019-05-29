7ea68bcf6352f2991bb1cefba587a224a684032e
VegSja <vgsjaavik@gmail.com>
2019-05-29 12:53:02 +0200

    Lagg til kommentarer

--- Rpi_music.py
+++ Rpi_music.py
@@ -3,26 +3,30 @@ import RPi.GPIO as GPIO
 import time
 import random
 
+#Setter opp GPIO pins på Raspberry Pi brettet
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Setter opp slik at pin 4 krever en lavere spenning for å aktiveres
 
-songs = ["/etc/Musicplayer/Sang.wav", "/etc/Musicplayer/TNT.wav", "/etc/Musicplayer/Minecraft_Style.wav"]
+#Deklarerer filplasseringen til de forskjellige sangene i en array
+songs = ["/etc/Musicplayer/blind.wav", "/etc/Musicplayer/stal.wav", "/etc/Musicplayer/subwoofer.wav", "/etc/Musicplayer/wait", "/etc/Musicplayer/wet"]
 
 def checkInput():
-    print(GPIO.input(4))	
+    print(GPIO.input(4)) 
+    #Sjekker om knappen er trykket ned
     if(GPIO.input(4)):
-            playSong()
+            playSong() 
     else:
        print("Waiting for buttonpress")
        os.system("clear")
     time.sleep(1)
+#Velger sang tilfeldig
 def playSong():
-    songNumber = random.randint(0,2)
-    os.system("play" + songs[songNumber])  
-    os.system("echo 'finish'")
+    songNumber = random.randint(0,5) #Generer tilfedig tall mellom 0 og 5
+    os.system("play" + songs[songNumber]) #Velger sang fra tidligere array basert på tallet songNumber og bruker kommandoen play for å spille sangen.
+    os.system("echo 'finish'") #Skriver finish når er ferdig.
 
 while True:
-	checkInput()
+	checkInput() #Kjører checkInput hele tiden når koden kjører
 
         
